6f6ffb14a73e5273ed99d267b79b724cfc44ab78
shenjinqi <183998158@qq.com>
2018-06-21 15:31:56 +0800

    flask-restful with blueprint

--- docker_nginx_flask_gunicorn/app/flaskapp/views/education.py
+++ docker_nginx_flask_gunicorn/app/flaskapp/views/education.py
@@ -1,9 +1,18 @@
 from flask import Blueprint, render_template, jsonify
+from flask_restful import Api, Resource, reqparse
 
 mod = Blueprint('education', __name__, url_prefix='/education')
+api = Api(mod)
 
 
 @mod.route('/')
 def index():
     return render_template('education/index.html')
+   
+    
+class Hi(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
+
+api.add_resource(Hi, "/hi")
